3d73f67a649c1b653c031d22af3a29c9ba91eba3
Konoval <MKonoval@kiev.luxoft.com>
2019-02-19 18:00:44 +0200

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoURI: "mongodb://admin:root1@206.189.2.72:27017/merndb",
-    localMongoURI: "mongodb://localhost:27017/natbdb"
+    localMongoURI: "mongodb://localhost:27017/merndb"
 }
