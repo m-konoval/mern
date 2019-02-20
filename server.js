fe3b433413e023e7dd6dfe5e2f8691d2d7fa47d8
Konoval <MKonoval@kiev.luxoft.com>
2019-02-20 12:26:48 +0200

    fix

--- server.js
+++ server.js
@@ -37,10 +37,10 @@ app.use('/api/items', Items);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('public'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
     });
 }
 
