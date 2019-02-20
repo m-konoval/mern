3aaad041b959abe93f2bd0bbfcddb01dcdc948c2
Konoval <MKonoval@kiev.luxoft.com>
2019-02-20 14:03:21 +0200

    fix store

--- client/src/store.js
+++ client/src/store.js
@@ -5,13 +5,13 @@ import rootReducer from './reducers';
 const initialState = {};
 
 const middleware = [thunk];
-const devTools = process.env.NODE_ENV === 'development'
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-    : null
+// dev tools middleware
+const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
 
-const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleware),
-    devTools
+const store = createStore(rootReducer, initialState, composeSetup(
+    applyMiddleware(...middleware)
 ));
 
 
