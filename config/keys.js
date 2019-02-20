27124e38737160576ca0633dd7c0aeff1e705c0c
Konoval <MKonoval@kiev.luxoft.com>
2019-02-20 13:05:00 +0200

    fix

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
     mongoURI: "mongodb://admin:root1@206.189.2.72:27017/merndb",
-    localMongoURI: "mongodb://localhost:27017/merndb"
+    localMongoURI: "mongodb://127.0.0.1:27017/merndb"
 }
